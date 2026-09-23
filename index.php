ea5a472111733ee9e5a8f37e33e8e7fa3f94a378
Kuba <benekwala@gmail.com>
2026-09-23 22:55:33 +0200

    Coś

--- index.php
+++ index.php
@@ -1,9 +1,34 @@
 <?php
-$array = [
-[1,2,3,4],
-[5,6,7,8]
+$array = [1,2,3,4,5,6,7];
+print_r(coś($array));
+function coś($array){
+    $sum = 0;
+    $średnia = 0;
+    $ilość = 0;
+    $mini = $array[0];
+    $maks = $array[0];
+    for($i = 0; $i < count($array); $i++){
+        $sum = $sum + $array[$i];
+    }
+    $średnia = $sum / count($array);
+    for($i = 0; $i < count($array); $i++){
+        if($array[$i] < $mini){
+            $mini = $array[$i];
+        }
+    }
+    for($i = 0; $i < count($array); $i++){
+        if($array[$i] > $maks){
+            $maks = $array[$i];
+        }
+    }
+    $wynik = [
+    "suma" => $sum,
+    "srednia" => $średnia,
+    "minimum" => $mini,
+    "maksimum" => $maks
 ];
 
-echo $array[0][2];
-echo "Pan jest po prostu niemieckim agentem"
+return $wynik;
+    
+}
 ?>
