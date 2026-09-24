9ae586a4eda6869a6ccfe02fc36bb924e9ed6154
Votomi <benekwala@gmail.com>
2026-09-24 13:47:03 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,34 +1,38 @@
 <?php
-$array = [1,2,3,4,5,6,7];
-print_r(coś($array));
-function coś($array){
-    $sum = 0;
-    $średnia = 0;
-    $ilość = 0;
-    $mini = $array[0];
-    $maks = $array[0];
-    for($i = 0; $i < count($array); $i++){
-        $sum = $sum + $array[$i];
-    }
-    $średnia = $sum / count($array);
-    for($i = 0; $i < count($array); $i++){
-        if($array[$i] < $mini){
-            $mini = $array[$i];
-        }
-    }
-    for($i = 0; $i < count($array); $i++){
-        if($array[$i] > $maks){
-            $maks = $array[$i];
-        }
-    }
-    $wynik = [
-    "suma" => $sum,
-    "srednia" => $średnia,
-    "minimum" => $mini,
-    "maksimum" => $maks
-];
 
-return $wynik;
-    
+function obliczStatystyki($liczby)
+{
+    $suma = 0;
+    $min = $liczby[0];
+    $max = $liczby[0];
+
+    foreach ($liczby as $liczba) {
+        $suma += $liczba;
+
+        if ($liczba < $min) {
+            $min = $liczba;
+        }
+
+        if ($liczba > $max) {
+            $max = $liczba;
+        }
+    }
+
+    $srednia = $suma / count($liczby);
+
+    return [
+        "min" => $min,
+        "max" => $max,
+        "suma" => $suma,
+        "srednia" => $srednia
+    ];
 }
-?>
+
+$liczby = [10, 5, 8, 20, 7];
+
+$wynik = obliczStatystyki($liczby);
+
+echo "Najmniejsza: " . $wynik["min"] . PHP_EOL;
+echo "Największa: " . $wynik["max"] . PHP_EOL;
+echo "Suma: " . $wynik["suma"] . PHP_EOL;
+echo "Średnia: " . $wynik["srednia"] . PHP_EOL;
